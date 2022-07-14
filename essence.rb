04a54150f0f78de3f2711fd6ce86c10f86393cf2
dmitry <shudv@yandex.ru>
2022-07-14 16:49:59 +0300

    preparing for the first test

--- essence.rb
+++ essence.rb
@@ -1,3 +1,10 @@
 class Essence
   
+  def initialize (name, health = 100)
+    @name = name.capitalize
+    @health = health
+    end
 end
+
+n = Essence.new('mike')
+puts n.inspect
--- example_rspec.rb
+++ example_rspec.rb
@@ -1,5 +1,10 @@
 require './essence.rb'
 
 describe Essence do
+  
+  def initilaz (name, health = 100)
+  @name = name.capitalize
+  @health = health
+  end
 
 end
