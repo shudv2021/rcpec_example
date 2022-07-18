e4d1e47d440c59dea19db00dade0b4655caa3704
dmitry <shudv@yandex.ru>
2022-07-18 11:19:12 +0300

    refactoring test

--- example_rspec.rb
+++ example_rspec.rb
@@ -2,22 +2,22 @@ require './essence.rb'
 
 describe Essence do
   
+  before do
+    @essence = Essence.new('man')
+  end
   
   it "Has capitalized name" do
-    essence = Essence.new 'man'
-    expect(essence.name).to eq 'Man'
+    expect(@essence.name).to eq 'Man'
   end
 
   it "Has decrease teen pount ennergy" do
-    essence = Essence.new 'man'
-    essence.decrease_energy
-    expect(essence.energy).to eq 90
+    @essence.decrease_energy
+    expect(@essence.energy).to eq 90
   end
 
   it "Has add energy" do
-    essence = Essence.new 'Name'
-    essence.add_energy
-    expect(essence.energy).to eq 110
+    @essence.add_energy
+    expect(@essence.energy).to eq 110
   end
 end
 
